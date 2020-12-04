f04b32a33f75ecf8a20b8c153ad8ec3f94e3d024
harsh6372 <58566945+harsh6372@users.noreply.github.com>
2020-12-04 22:12:05 +0530

    python_logic_added

---
+++ Python logic/player.py
@@ -0,0 +1,75 @@
+from card import Card
+from details import heirarchies, numbers, suits, gameTypes
+
+
+class Player:
+    def __init__(self):
+        self.cards = []
+        self.hierarchy = None
+        self.hierarchyNumber = None
+
+    def suitChecker(self):
+        """Returns True is cards suit is same"""
+        if self.cards[0].suit == self.cards[1].suit == self.cards[2].suit:
+            return True
+
+    def numberChecker(self):
+        """Returns True if all cards have same numbers"""
+        if self.cards[0].number == self.cards[1].number == self.cards[2].number:
+            return True
+
+    def sequenceChecker(self):
+        """Returns True if the cards form any sequence and sets sequenceHierarchy"""
+        l = [card.number for card in self.cards]
+        l.sort()
+        """Considering A23 the highest sequence"""
+        if l == [2, 3, 14]:
+            self.hierarchyNumber = [12]
+            return True
+        elif l[2] - l[0] == 2:
+            self.hierarchyNumber = [l[0] - 1]
+            return True
+        return False
+
+    def pairChecker(self):
+        """Checks if cards contain a pair and sets hierarchyNumber to a list
+        where first element is pair number and second is number of the remaining card"""
+        l = [card.number for card in self.cards]
+        l.sort()
+        if l[1] == l[0]:
+            self.hierarchyNumber = [l[1], l[2]]
+            return True
+        elif l[1] == l[2]:
+            self.hierarchyNumber = [l[1], l[0]]
+            return True
+        return False
+
+    def hierarchy_decider(self):
+        if self.numberChecker():
+            self.hierarchy = heirarchies["Trail"]
+            self.hierarchyNumber = [self.cards[0].number]
+
+        elif self.sequenceChecker():
+            if self.suitChecker():
+                self.hierarchy = heirarchies["PureSeq"]
+            else:
+                self.hierarchy = heirarchies["Seq"]
+        elif self.suitChecker():
+            self.hierarchy = heirarchies["Color"]
+            cards = [card.number for card in self.cards]
+            cards.sort()
+            self.hierarchyNumber = list(reversed(cards))
+        elif self.pairChecker():
+            self.hierarchy = heirarchies["Pair"]
+        else:
+            self.hierarchy = heirarchies["HighCard"]
+            cards = [card.number for card in self.cards]
+            cards.sort()
+            self.hierarchyNumber = list(reversed(cards))
+
+# Test
+# l=[Card(0,'a'),Card(1,"a"),Card(2,"a")]
+# p=Player()
+# p.cards=l
+# p.hierarchy_decider()
+# print(p.hierarchy,p.hierarchyNumber)
---
+++ Python logic/solver.py
@@ -0,0 +1,116 @@
+from player import Player
+from card import Card
+from details import heirarchies, numbers, suits, gameTypes
+import random
+
+
+def winnerAction(winner):
+    if winner != 0:
+        print("Player", winner, "won.")
+    else:
+        print("Tie")
+
+
+class game:
+    def __init__(self, gameType):
+        """gameType is a string"""
+        self.gameType = gameType
+        self.cardsDealt = gameTypes[gameType]
+        self.P1 = Player()
+        self.P2 = Player()
+
+    def setCard(self, player_id, suit, num):
+        """appends card object to player of given player_id
+        inputs are player_id,suit code(0,1,2,3) and num(2,3,4,'a','k','q')"""
+        if player_id == 1:
+            if len(self.P1.cards) < self.cardsDealt:
+                self.P1.cards.append(Card(suit, num))
+            else:
+                if len(self.P2.cards) < self.cardsDealt:
+                    """We can automatically switch to player 2"""
+                    print("Enter cards for player 2")
+                else:
+                    """We can disable the enter button and activate the compare button"""
+                    print("Initiate compare")
+        elif player_id == 2:
+            if len(self.P2.cards) < self.cardsDealt:
+                self.P2.cards.append(Card(suit, num))
+            else:
+                if len(self.P1.cards) < self.cardsDealt:
+                    """We can automatically switch to player 1"""
+                    print("Enter cards for player 1")
+                else:
+                    """We can disable the enter button and activate the compare button"""
+                    print("Initiate compare")
+        return True
+
+    def gameType_Classic(self):
+        p1 = [self.P1.hierarchy] + self.P1.hierarchyNumber
+        p2 = [self.P2.hierarchy] + self.P2.hierarchyNumber
+        for i in range(len(p1)):
+            if p1[i] > p2[i]:
+                winnerAction(1)
+                break
+            elif p1[i] < p2[i]:
+                winnerAction(2)
+                break
+
+    def gameTypeMethodDecider(self):
+        if self.gameType == "Classic":
+            self.gameType_Classic()
+
+    def compare(self):
+        if len(self.P1.cards) == self.cardsDealt and len(self.P2.cards) == self.cardsDealt:
+            self.P1.hierarchy_decider()
+            self.P2.hierarchy_decider()
+            self.gameTypeMethodDecider()
+        else:
+            print("Enter More cards")
+
+
+"""Stream Looks like this"""
+# gT = "Classic"
+# g = game(gT)
+# g.compare()
+# g.setCard(1, "spade", 2)
+# g.setCard(1, "spade", "a")
+# g.setCard(1, "spade", 3)
+# g.setCard(2, "spade", "a")
+# g.setCard(2, "spade", "k")
+# g.setCard(2, "spade", "q")
+# g.compare()
+
+"""Python Testing Code"""
+# testing_iterations = 1
+# s = [(k) for k, v in suits.items()]
+# n = [(k) for k, v in numbers.items()]
+# for i in range(testing_iterations):
+#     p1, p2 = [], []
+#     gT = "Classic"
+#     g = game(gT)
+#     cards = []
+#     for j in range(3):
+#         t, suit, num = True, None, None
+#         while t:
+#             suit = random.choice(s)
+#             num = random.choice(n)
+#             if [suit, num] not in cards:
+#                 cards.append([suit, num])
+#                 break
+#             print(1)
+#         t = g.setCard(1, suit, num)
+#         p1 += [[suit, num]]
+#     for j in range(3):
+#         t, suit, num = True, None, None
+#         while t:
+#             suit = random.choice(s)
+#             num = random.choice(n)
+#             if [suit, num] not in cards:
+#                 cards.append([suit, num])
+#                 break
+#             print(2)
+#         t = g.setCard(2, suit, num)
+#         p2 += [[suit, num]]
+#     print(p1, p2)
+#     print(cards)
+#     g.compare()
